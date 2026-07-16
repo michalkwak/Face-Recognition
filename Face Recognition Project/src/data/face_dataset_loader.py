f999b09ed52d83a4d10f732425aa4f96835d0156
Michał Kwak <michal.kwak2806@gmail.com>
2026-07-16 18:57:36 +0300

    Created a visualization script

--- Face Recognition Project/src/data/face_dataset_loader.py
+++ Face Recognition Project/src/data/face_dataset_loader.py
@@ -1,26 +1,7 @@
 from sklearn.datasets import fetch_olivetti_faces
-import matplotlib.pyplot as plt
-from math import ceil, sqrt
 
 def load_olivetti_faces():
     """Load the Olivetti faces dataset."""
     faces = fetch_olivetti_faces()
     return faces.images
 
-def showImgs(imgs, n_imgs, i_imgs):
-    rows = ceil(sqrt(n_imgs))
-    cols = ceil(n_imgs / rows)
-
-    fig = plt.figure(figsize=(10, 10))
-
-    for p, i in enumerate(i_imgs, start=1):
-        fig.add_subplot(rows, cols, p)
-        plt.imshow(imgs[i], cmap="gray")
-        plt.axis("off")
-
-    plt.tight_layout()
-    plt.show()
-
-if __name__ == "__main__":
-    images = load_olivetti_faces()
-    showImgs(images, 100, range(100))
