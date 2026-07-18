b1d6cd0ff3022acf2c104190077d7e7b509160ed
Michał Kwak <michal.kwak2806@gmail.com>
2026-07-18 23:59:26 +0300

    Update jacobi.py

--- Face Recognition Project/src/linalg/jacobi.py
+++ Face Recognition Project/src/linalg/jacobi.py
@@ -1,10 +1,10 @@
 import numpy as np
+from math import sqrt
 
 def jacobi_eigenvalue(A, tol=1e-14, max_iter=1000):
     '''
-    Diagonalizes a symmetric matrix using the Jacobian method
-
-    At each step, finds the largest off-diagonal element and applies
+    Diagonalizes a symmetric matrix using the Jacobian method, that is, st each step, 
+    finds the largest off-diagonal element and applies
     a rotation that zeroes it out, repeating until all off-diagonal
     elements are below "tol".
 
@@ -23,18 +23,35 @@ def jacobi_eigenvalue(A, tol=1e-14, max_iter=1000):
 def max_off_diagonal(A, n):
     '''Returns the largest off-diagonal element of A[p, q] where p < q'''
     max_val = 0.0
-    p, q = 0, 1
+    k, l = 0, 1
 
     for i in range(n-1):
         for j in range(i+1, n):
             if abs(A[i, j]) > max_val:
                 max_val = abs(A[i, j])
-                p, q = i, j
-    return max_val, p, q
+                k, l = i, j
+    return max_val, k, l
 
-def rotate(a, v, n, q, p):
+def rotate(a, v, n, k, l):
     '''Rotate A in place to eliminate A[p, q]'''
-    pass
+    diff = a[l, l] - a[k, k]
+
+    if abs(a[k, l]) < abs(diff) * 1.0e-10:
+        t = a[k, l] / diff
+    else:
+        phi = diff / (2.0 * a[k, l])
+        t = 1.0 / (abs(phi) + sqrt(phi ** 2 + 1.0))
+        if phi < 0.0:
+            t = -t
+
+    c = 1.0 / sqrt(t ** 2 + 1.0)
+    s = t * c
+    tau = s / (1.0 + c)
+
+    a_kl = a[k, l]
+    a[k, l] = 0.0
+    a[k, k] -= t * a_kl
+    a[l, l] += t * a_kl
 
 def rotate_pair(a, row1, col1, row2, col2, s, tau):
     pass
